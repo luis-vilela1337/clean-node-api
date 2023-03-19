b965f95eb23e59519bdbbe82d0cb5e549addd085
Luis Fillype Vilela <l.fillype@gmail.com>
2023-03-18 21:14:08 -0300

    feat: ensure SignUpController call AddAccount with correct values

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,15 +1,17 @@
 import { type HttpRequest, type HttpResponse, type EmailValidator, type Controller } from '../protocols'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { InvalidParamError, MissingParamError } from '../errors'
+import { type AddAccount } from '../../domain/usecases/add-account'
 
 export class SignUpController implements Controller {
-  constructor (private readonly _emailValidator: EmailValidator) {
-
+  constructor (
+    private readonly _emailValidator: EmailValidator,
+    private readonly _addAccount: AddAccount) {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
-      const { email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
@@ -25,6 +27,9 @@ export class SignUpController implements Controller {
       if (!isValidEmail) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      const account = this._addAccount.add({ name, email, password })
+
       return {
         statusCode: 200,
         body: 'Success'
