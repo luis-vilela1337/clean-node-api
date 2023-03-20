866d0d3947a3216a8d45e86c5563958bfc2f9e90
Luis Fillype Vilela <l.fillype@gmail.com>
2023-03-19 21:49:31 -0300

    feat: ensur BcryptAdapter returns a hash on success

--- src/infra/cripotography/bcrypt-adapter.ts
+++ src/infra/cripotography/bcrypt-adapter.ts
@@ -6,7 +6,6 @@ export class BcryptAdapter implements Encrypter {
   }
 
   async encrypt (value: string): Promise<string> {
-    await bcrypt.hash(value, this._salt)
-    return 'null'
+    return await bcrypt.hash(value, this._salt)
   }
 }
