dfea7d367940ccc96dc4f1dbb1e86a6f48fc1fae
Luis Fillype Vilela <l.fillype@gmail.com>
2023-03-19 21:49:38 -0300

    test: ensur BcryptAdapter returns a hash on success

--- src/infra/cripotography/bcrypt-adapter.spec.ts
+++ src/infra/cripotography/bcrypt-adapter.spec.ts
@@ -1,18 +1,39 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
-const salt = 12
-const makeSut = () => {
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await Promise.resolve('hashad-value')
+  }
+}))
 
+interface SutTypes {
+  sut: BcryptAdapter
+}
+
+const salt = 12
+const makeSut = (): SutTypes => {
+  const sut = new BcryptAdapter(salt)
+  return {
+    sut
+  }
 }
 describe('Bcrypt Adapter', () => {
   describe('Success tests', () => {
     test('Should call bcrypt with correct values', async () => {
-      const sut = new BcryptAdapter(salt)
+      const { sut } = makeSut()
       const hashSpy = jest.spyOn(bcrypt, 'hash')
       await sut.encrypt('any-value')
 
       expect(hashSpy).toHaveBeenCalledWith('any-value', salt)
+    })
+
+    test('Should return a hash on success', async () => {
+      const { sut } = makeSut()
+
+      const response = await sut.encrypt('any-value')
+
+      expect(response).toEqual('hashad-value')
     })
   })
   describe('Failed tests', () => {})
